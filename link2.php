de7ee810af2cc4e5cdd523fdf94d366a59383413
lazartemabel <mabel_tqm@hotmail.com>
2015-01-16 19:57:12 -0500

    Create link2.php

---
+++ link2.php
@@ -0,0 +1,90 @@
+<?php include ('parte1.php'); ?>
+<div align="center" class="texto1">
+ <h1 id="title" align="center"> CONSEJOS DE SALUD </h1>
+   
+ <hr>
+<table width="766" border="0" cellpadding="0" cellspacing="0">
+
+
+<tr>
+  <td colspan="2" bgcolor="#FFFFFF">
+  
+  <div align="justify" class="texto">
+    <h1 align="left" class="Estilo1">Que es el Flujo Vaginal??</h1>
+    <div align="left"><span class="Estilo1"><img src="img/line.jpg" width="300" height="4" /><br />
+      </span>    </div>
+    <div align="justify" class="texto1 Estilo2">
+      <p align="left">El flujo vaginal es  una secreción proveniente de la vagina y se presenta en todas las mujeres  aunque con una diferencia, para unas ésta secreción es más consistente, para  otras es de color e incluso puede variar en el olor.<br />
+        El hecho de que una mujer tenga  abundante flujo es en realidad normal, especialmente si ella esta digamos que  en una temporada fértil, sin embargo, un&nbsp;<strong>flujo vaginal</strong>&nbsp;que  súbitamente cambia de color, olor o consistencia puede ser indicador de que hay  una infección.<br />
+        Aspectos  como los siguientes pueden hacer que una mujer aumente su flujo vaginal, sin  motivos para preocuparse:</p>
+      <ul>
+        <li>Estrés  emocional </li>
+        <li>Ovulación</li>
+        <li>Embarazo</li>
+        <li>Excitación sexual</li>
+      </ul>
+      <p>Sin embargo, otras razones por las que el&nbsp;<strong>flujo  vaginal</strong>&nbsp;puede  aumentar y a las que sí vale la pena poner atención son:</p>
+      <ul>
+        <li>Vaginitis atrófica</li>
+        <li>Enfermedades de transmisión sexual</li>
+        <li>Infecciones</li>
+        <li>Candidiasis</li>
+        <li>Tampón por demasiadas horas</li>
+      </ul>
+      <h2 style="font-size: 14px">Prevenir  y tratar el flujo vaginal</h2>
+      <p>Las siguientes son un par de  recomendaciones para<strong>&nbsp;quitar el flujo vaginal</strong>, toma  nota y convérsalas con tu médico:&nbsp;</p>
+      <ul>
+        <li>Mantén el área genital limpia y seca.</li>
+        <li>No tomes duchas vaginales, puede ser más riesgo para adquirir  infecciones.</li>
+        <li>Si tienes candidiasis utiliza cremas vaginales.</li>
+        <li>Toma yogur con cultivos vivos o tabletas de Lactobacillus acidophilus  cuando estés tomando antibióticos para evitar una candidiasis vaginal.</li>
+        <li>Usa preservativo en tus relaciones sexuales.</li>
+        <li>Evita el uso de aerosoles, fragancias o polvos en los genitales.</li>
+        <li>Usa ropa interior de algodón. Evita la ropa de seda.</li>
+        <li>Usa protectores y cámbialos en tiempos adecuados.</li>
+        <li>Evita la ropa demasiado ajustada.</li>
+      </ul>
+      <p>Si además del flujo  vaginal presentas síntomas como fiebre, dolor en la pelvis, pérdida de peso,  mucha sed, inapetencia, aumento en la frecuencia de la micción o fatiga, lo  ideal es que consultes a tu médico inmediatamente.</p>
+    </div>
+  </div></td>
+  </tr>
+  <tr>
+  <td colspan="2" bgcolor="#FFFFFF">
+  
+  <div align="justify" class="texto">
+    <h1>Dieta de la chia para perder  kilos rapido</h1>
+    <div align="left"><span class="Estilo1"><img src="img/line.jpg" width="300" height="4" /><br />
+      </span>    </div>
+    <div align="justify" class="texto1 Estilo2">
+      <p>La chía es una de las semillas que, en los últimos tiempos, ha  tomado mucho interés; gracias a sus propiedades nutricionales y medicinales.  Por ello, porque no aprovechar sus beneficios&nbsp;<strong>laxantes para desintoxicar el cuerpo</strong>,  y desarrollar una&nbsp;<strong>dieta</strong>&nbsp;que te permita&nbsp;<strong>perder  kilos rápido</strong>.<br />
+Las semillas, por sí mismas, tienen un&nbsp;<strong>efecto  laxante</strong>&nbsp;que ayuda  a <strong>desintoxicar el  organismo de forma natural</strong>. No obstante, la chía es una semilla  que posee otras propiedades que no sólo te permiten&nbsp;<strong>perder  peso</strong>, sino prevenir ciertas&nbsp;<strong>complicaciones propias de la obesidad</strong>,  como el colesterol alto.<br />
+Por esta razón, si estás buscando <strong>adelgazar rápido</strong>,  alimentarte bien y recibir nutrientes que favorezcan tu&nbsp;<strong>salud  cardiovascular</strong>, ten en cuenta la&nbsp;<strong>dieta  de la chía</strong>.<br />
+<strong>Dieta de la chía para perder peso rápido</strong> </p>
+      <ul>
+        <li><strong>Desayuno</strong>: 1  Taza de&nbsp;té verde&nbsp;con una rodaja de limón. 1 vaso de licuado proteico, con una  cucharadita de semillas de chía.</li>
+        <li><strong>Media Mañana</strong>:  bastoncitos de zanahoria o apio.</li>
+        <li><strong>Almuerzo</strong>: 1  taza de caldo casero con una cucharadita de semillas de chía. Ensalada de atún,  huevo duro y arroz integral. 1 fruta fresca.</li>
+        <li><strong>Merienda</strong>: 1  yogur desnatado con cereales integrales.</li>
+        <li><strong>Media tarde</strong>: 1  trozo de queso bajo en grasas.</li>
+        <li><strong>Cena</strong>: 1  taza de caldo casero con una cucharadita de semillas de chía. Pescado a la  parrilla con vegetales al wok. 1 porción de gelatina light.</li>
+      </ul>
+      <p>Este es un&nbsp;<strong>ejemplo de menú</strong>, en  el cual puedes observar cómo se distribuye a lo largo del día&nbsp;<strong>15  gr</strong>&nbsp;(1 cucharada sopera)&nbsp;<strong>de chía</strong>. </p>
+    </div>
+  </div></td>
+  </tr>
+  <tr>
+  <td colspan="2" bgcolor="#FFFFFF">
+  
+  <div align="justify" class="texto">
+    <h1 align="left" class="Estilo1">Como Lograr un Vientre Plano </h1>
+    <div align="left"><span class="Estilo1"><img src="img/line.jpg" width="300" height="4" /><br />
+      </span>    </div>
+    <div align="justify" class="texto1 Estilo2">
+      <p><strong>Para perder peso  rápidamente, y conseguir un vientre plano</strong>,  no existen productos milagrosos; si quieres conseguir un vientre plano,<strong>&nbsp;</strong><strong>debes de cambiar tus hábitos y ser constante</strong>.</p>
+      <p><strong>Haz  combinaciones de entrenamiento de alta intensidad, con ejercicios  cardiovasculares</strong>&nbsp; tres días o la semana, para maximizar la quema de grasa;  alterna entre varios minutos de ejercicio cardiovascular a una intensidad  fácil, y varios minutos de cardio de alta intensidad. Realiza sesiones de 30  minutos.      </p>
+      <p><img src="images/am_97918_1902544_468391.jpg" width="1024" height="561"></p>
+    </div>
+  </div></td>
+  </tr>
+</table>
+</div>
