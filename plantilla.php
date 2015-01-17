59c7948e10323d5e950fe93c3d7d0db9945bdc3c
lazartemabel <mabel_tqm@hotmail.com>
2015-01-16 19:58:41 -0500

    Create plantilla.php

---
+++ plantilla.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>NOTICIAS | TACNA</title>
+<link href="css/css.css" rel="stylesheet" type="text/css" />
+<script src="Scripts/swfobject.js" type="text/javascript" language="javascript"></script>
+</head>
+<body>
+<div align="center">
+<table width="766" border="0" cellpadding="0" cellspacing="0">
+<tr>
+<td colspan="2">
+<div id="flashcontent_cabecera">
+       <center>
+        <a target="_blank" href="http://www.macromedia.com/go/getflash/"></a>
+      </center>
+    </div>   
+       <script>   
+      var so = new SWFObject("flash/cabecera.swf", "cabecera", "766", "370", "8.0.0.0", "#FFCCFF"); 
+      so.addParam("quality", "high"); 
+      so.addParam("scale", "scale"); 
+      so.addParam("cabecera", "false"); 
+      so.addParam("salign", "lt"); 
+      so.addParam("wmode", "transparent");
+      so.addParam("flashvars", "configfile=3dcarousel.xml");
+      so.write("flashcontent_cabecera");	  
+    <!-- END embed 3D Carousel --> 
+        </script>
+</td>
+</tr>
+<tr>
+  <td width="266">&nbsp;</td>
+  <td width="500">&nbsp;</td>
+</tr>
+<tr>
+  <td colspan="2"><p>este sera el contenido</p>
+    <p>&nbsp;</p></td>
+  </tr>
+<tr>
+  <td>&nbsp;</td>
+  <td>&nbsp;</td>
+</tr>
+
+</table>
+</div>
+</body>
+</html>
