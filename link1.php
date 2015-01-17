022c8e7442b6f1ec1f6b5395cae0e4b4c439eabb
ana marcela <ana.marcela.cairo@gmail.com>
2015-01-16 19:04:31 -0500

    Create link1.php
    Página de Tips de Belleza

---
+++ link1.php
@@ -0,0 +1,76 @@
+<?php include ('parte1.php'); ?>
+
+<div align="center" class="texto1">
+ <h1 id="title" align="center"> TIPS DE BELLEZA </h1>
+   
+ <hr>
+<table width="766" border="0" cellpadding="0" cellspacing="0">
+
+
+<tr>
+  <td colspan="2" bgcolor="#FFFFFF">
+  
+  <div align="justify" class="texto">
+    <h1 align="left" class="Estilo1">DILE ADIÓS AL ACNÉ CON ESTOS REMEDIOS NATURALES </h1>
+    <div align="left"><span class="Estilo1"><img src="img/line.jpg" width="535" height="4" /><br />
+      </span>
+    </div>
+    <div align="justify" class="texto1 Estilo2">
+      <p><strong>1. Cáscara de naranja</strong><br />
+Una de las propiedades de la naranja es su poder astringente que ayuda a   eliminar los excesos de grasa en la piel y todo tipo de impurezas.   Preparar un tónico casero de naranja es ideal para empezar a combatir la   piel grasa y el acné. <strong>Lo único que tienes que hacer es machacar un poco de cáscara de naranja y mezclarla con agua. </strong>Se aplica en el rostro antes de dormir exclusivamente en las zonas afectadas.</p>
+      <p><strong>2. Ajo</strong><br />
+        El ajo es un alimento lleno de propiedades medicinales, entre las que se   incluye sus propiedades desinfectantes y antibacteriales  que ayudan a   combatir las bacterias que causan el acné. <strong>Este ingrediente se   ha mostrado muy efectivo en el tratamiento para el acné, pues ayuda a   combatir el exceso de grasa y hace que el rostro recupere frescura.</strong> Si no te parece un problema su fuerte olor, lo recomendado es frotar un   ajo rebanado sobre las zonas afectadas varias veces al día. </p>
+      <p><strong>3. Pepino</strong><br />
+        El alto contenido de agua y vitaminas del pepino, lo convierten en un   ingrediente ideal para hidratar la piel y mantenerla fresca. <strong>Sus contenidos de vitamina C y ácido ascórbico lo convierten en un gran aliado para combatir el acné.</strong> Si deseas aprovechar las propiedades del pepino, toma ½ pepino,   retírale las semillas y procésalo muy bien en la licuadora. La pasta   resultante la aplicas en el rostro durante 20 minutos y retiras con agua fría.</p>
+      <p align="center"><img src="img/belleza/acne.jpg" alt="Acne" width="259" height="165" longdesc="img/belleza/acne.jpg" /></p>
+      <p>&nbsp;</p>
+    </div>
+  </div></td>
+  </tr>
+  <tr>
+  <td colspan="2" bgcolor="#FFFFFF">
+  
+  <div align="justify" class="texto">
+    <h1 align="left" class="Estilo1">REMEDIOS CASEROS PARA ACABAR CON LAS BOLSAS EN LOS OJOS</h1>
+    <div align="left"><span class="Estilo1"><img src="img/line.jpg" width="640" height="4" /><br />
+    </span> </div>
+    <div align="justify" class="texto1 Estilo2">
+      <p><span style="font-weight: bold">1.</span> <strong>Duerme y descansa lo necesario:</strong> una de las   principales causas de las bolsas en los ojos y ojeras, es la falta de   descanso. Es muy importante dormir al menos 8 horas diarias sin   interrupciones y realizar regularmente actividades que te relajen.</p>
+      <p><span style="font-weight: bold">2. </span><strong>Utiliza una o dos almohadas: </strong>dormir con la cabeza   más alta que el cuerpo es ideal para descansar mejor y evitar que el   líquido se acumule con más facilidad en la zona facial. Es recomendado   utilizar una o dos almohadas para evitar los ojos hinchados.</p>
+      <p><span style="font-weight: bold">3. </span><strong>Consume alimentos diuréticos y baja el consumo de sal: </strong>una   cena a base de frutas y verduras puede ser una gran forma para eliminar   los líquidos que hay retenidos en el cuerpo. Incluir a diario más   alimentos diuréticos y reducir el consumo de sal, es ideal para combatir   la retención de líquidos y tener un mejor aspecto en los ojos.</p>
+      <p><span style="font-weight: bold">4.</span> <strong>Aplicar agua fría en las mañanas:</strong> casi siempre   vemos que los ojos están hinchados en las horas de la mañana. Para esto   es recomendado lavar la cara con agua bien fría o en lo posible aplicar   hielo en la zona afectada.</p>
+      <p><span style="font-weight: bold">5.</span> <strong>Utilizar pepino frío: </strong>el pepino es un hidratante   natural de la piel y nos ayuda a lucir un mejor aspecto. Poner algunas   rodajas de pepino en el congelador y después aplicarlas en la zona de   las bolsas, ayuda a reducir la inflamación y a lucir más frescas.</p>
+      <p align="center"><img src="img/belleza/p_gallo.jpg" alt="bolsas" width="259" height="165" longdesc="img/belleza/p_gallo.jpg" /></p>
+    </div>
+    <div align="justify" class="texto1 Estilo2"></div>
+  </div></td>
+  </tr>
+  <tr>
+  <td colspan="2" bgcolor="#FFFFFF">
+  
+  <div align="justify" class="texto">
+    <h1 align="left" class="Estilo1">CONSEJOS Y REMEDIOS PARA HACER CRECER EL CABELLO</h1>
+    <div align="left"><span class="Estilo1"><img src="img/line.jpg" width="565" height="4" /><br />
+      </span>
+    </div>
+    <div align="justify" class="texto1 Estilo2">
+      <p><span style="font-weight: bold">1. </span><strong>Estimula la circulación de tu cuero cabelludo: </strong>para esto sólo basta con cepillar unas 50 veces con movimientos suaves y uniformes, desde la raíz hasta las puntas.</p>
+      <p><span style="font-weight: bold">2.</span> <strong>Realiza masajes capilares:</strong> todas las noches antes   de ir a dormir, es recomendado hacerse un masaje capilar de 10 minutos.   Para esto debes usar las palmas y dedos de tus manos con suaves masajes   circulares.</p>
+      <p><span style="font-weight: bold">3. </span><strong>Corta las puntas una vez al mes: </strong>las puntas del   cabello suelen sufrir más daños debido a los diferentes factores y es   por eso que es conveniente cortarlas regularmente.</p>
+      <p><span style="font-weight: bold">4.</span> <strong>Evita cepillar y/o peinar el cabello húmedo: </strong>cuando   el cabello está húmedo, es más débil y está más propenso a    quebrajarse. Lo recomendado es esperar a que se seque para peinarlo   cuidadosamente.</p>
+      <p><span style="font-weight: bold">5. </span><strong>No laves tu pelo todos los días:</strong> los expertos   recomiendan lavar el cabello de dos a tres veces por semana, ya que éste   tarda en recuperarse de los químicos del lavado. Descansar de los   diferentes productos, le permitirá al cabello producir aceites naturales   que ayudarán a su crecimiento.</p>
+      <p><span style="font-weight: bold">6. </span><strong>Evita el uso de productos con silicona:</strong> los   productos para el cabello basados en silicona, prometen un cabello más   saludable y sin daño. Sin embargo, se ha comprobado que este tipo de   productos pueden tapar los folículos impidiendo el crecimiento.</p>
+      <p align="center"><img src="img/belleza/cabello.jpg" alt="cabello" width="259" height="165" longdesc="img/belleza/cabello.jpg" /></p>
+    </div>
+  </div></td>
+  </tr>
+  <tr>
+  <td colspan="2" bgcolor="#FFFFFF">&nbsp;</td>
+  </tr>
+  
+  
+  
+
+
+</table>
+</div>
