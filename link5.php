d26653d40bf46ebfb55caf93141f75df514f5236
lcohaila <lynn528@gmail.com>
2015-01-16 19:55:46 -0500

    Create link5.php

---
+++ link5.php
@@ -0,0 +1,13 @@
+<?php include ('parte1.php'); ?>
+<div align="center" class="texto1">
+<h1>ESCRIBENOS</h1>
+<hr />
+<br />
+<?php include ('contact.php'); ?>
+<br>
+
+Correo Electrónico 
+<br>noticiastacna@hotmail.com
+<br>
+Teléfono: :052-777111
+</div>
