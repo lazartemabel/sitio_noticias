c2d33c1b4d3f5b2d207dabd33955cfebb0e7ac78
lcohaila <lynn528@gmail.com>
2015-01-16 19:52:20 -0500

    Create conexion1.php

---
+++ conexion1.php
@@ -0,0 +1,5 @@
+<?
+$link = mysql_connect("localhost", "root","sa") or die("Could not connect to database!"); 
+$basedatos = "noticias";
+mysql_select_db("noticias") or die("Could not select database!"); 
+?>
